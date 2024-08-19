09249ddf942e325f9f50ef4cc7228c48c021f489
Alexandre França <alexandre.franca@mercedes-benz.io>
2024-08-19 16:12:23 +0100

    Add base components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import ShowDetails from './components/ShowDetails.vue'
 </script>
 
 <template>
+  <!-- TODO: Implement routing -->
+  <ShowDetails />
 </template>
 
 <style scoped>
